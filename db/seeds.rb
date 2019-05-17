90c9d6c69dc7c00cc6f21406c82314dc60cff8bf
Marina <marinaillumkjaer@gmail.com>
2019-05-17 10:41:40 +0200

    pb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Deleting everything from database"
-Ingredient.destroy_all
-
 require 'json'
 require 'open-uri'
+
+puts "Deleting Ingredients from database..."
+Ingredient.destroy_all
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
